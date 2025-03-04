df68688119f8c5c659ec87d1e2caec089a6d68bf
hcc0507 <147717176+Huang-Chien-Chin@users.noreply.github.com>
2025-03-04 11:02:04 +0800

    version 8: PHP

---
+++ s1120326.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>黃千津簡介</title>
+	<style type="text/css">
+		* { font-family:"標楷體"; margin-left:auto; margin-right:auto;}
+		h1 {color:#99bbff; font-size:60px;}
+		h2 {color:#77ddff; font-size:40px;}
+	</style>
+
+	<script>
+		function change1() {
+  			document.getElementById("pic").src = "mountain.jpg";
+  			document.getElementById("h2text").innerText = "靜宜資管";
+		}
+
+		function change2() {
+  			document.getElementById("pic").src = "cliff.jpg";
+  			document.getElementById("h2text").innerText = "Chien-Chin Huang";
+		}
+	</script>
+
+
+</head>
+
+<body>
+	 <table width="70%">
+		<tr>
+			<td><img src="cliff.jpg" width="110%" id="pic" onmouseover="change1()" onmouseout="change2()"></img></td>
+
+			<td>
+				<h1>黃千津</h1>
+				<h2 id="h2text">Chien-Chin Huang</h2>
+			</td>
+
+		</tr>
+
+	</table>
+	
+	<table width="70%" border="1">
+		<tr>
+			<td>
+
+				個人網頁：<a href=file:///C:/Users/student/Desktop/s1120326.html>file:///C:/Users/student/Desktop/s1120326.html</a><br>
+				IG：<a href=https://www.instagram.com/cc._.725/>https://www.instagram.com/cc._.725/</a><br>
+				Tel: <a href="tel:0963587170">0963587170</a><br>
+				E-Mail: <a href="mailto:c0920434618@gmail.tw">c0920434618@gmail.com</a><br>
+
+			</td>
+
+			<td>
+
+				大象席地而坐電影配樂<br>
+					<audio controls>
+						<source src="elephant.mp3" type="audio/mP3">
+					</audio><br>
+
+			</td>
+
+			<td>
+
+				不要去臺灣<br>
+				<iframe src="https://www.youtube.com/embed/pW88QFpHXa8" allowfullscreen></iframe>
+
+			</td>
+		</tr>
+	</table>
+	<table width="70%" border="1">
+		<tr>
+			<td>
+				聊天機器人<br>
+				<iframe
+    				allow="microphone;"
+    				width="350"
+    				height="430"
+    				src="https://console.dialogflow.com/api-client/demo/embedded/c08c6829-6bbb-4034-97be-d6c2c7a4c13b">
+				</iframe>
+			</td>
+		</tr>
+	</table>
+	<?php echo data("Y-m-d")?>	
+</body>
+</html>
